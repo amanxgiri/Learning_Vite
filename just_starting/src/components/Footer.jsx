bf06ad19b3e9cf3faf8ce828ef7c9f7ffeb1f847
Aman Kumar Giri <amangiri.dev@gmail.com>
2026-07-03 00:17:50 +0530

    Just starting with vite

---
+++ just_starting/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+
+    return(
+        <footer>
+            <p>
+                copyright 2026 of Aman Inc.
+            </p>
+        </footer>
+    )
+}
---
+++ just_starting/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+
+    return (
+        <header>
+            <h1> Hello Vite App.</h1>
+        </header>
+    )
+}
