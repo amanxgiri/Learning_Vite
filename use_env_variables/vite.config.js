011c0f4f182f93811b765a970b7accbc79d0fd59
Aman Kumar Giri <amangiri.dev@gmail.com>
2026-07-03 12:38:23 +0530

    changed default port to 3000 and build to my directory

--- use_env_variables/vite.config.js
+++ use_env_variables/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // changed default port to 3000
+  server:{
+    port: 3000
+  },
+  // changed default build path from dist to out directory
+  build:{
+    outDir: "out"
+  }
 })
